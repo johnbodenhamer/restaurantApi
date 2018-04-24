14dcf2737ada40f4650432d87cbff2a34bc95fa2
Ryan Bodenhamer <ryan@Robs-MacBook.local>
2018-04-23 20:29:54 -0500

    second commit

---
+++ src/main/java/com/nonamegroup/restaurantApi/RestaurantApiApplication.java
@@ -0,0 +1,12 @@
+package com.nonamegroup.restaurantApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApiApplication.class, args);
+	}
+}
